4368bcede6267d1eb0821b930d64ad3d1d2b61c9
Ram Pareek <heyram.exe@gmail.com>
2025-07-12 10:44:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const { courseRouter } = require("./routers/course");
 const { adminRouter } = require("./routers/admin");
 
 app.use(express.json());
-app.use(express.static("public")); // ✅ this serves HTML/CSS/JS
+app.use(express.static("public")); 
 
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/course", courseRouter);
